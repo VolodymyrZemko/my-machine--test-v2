d6d13702ecd7f6dce7d0b9e7c080ecc1de4ef0d8
VolodymyrZemko <volodymyr.zemko@ua.nestle.com>
2026-06-22 15:28:37 +0300

    mobile tel changes, npm from 20 => 24, GH pages (url changed)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const isProd = mode === 'production';
   return {
     plugins: [react()],
-    base: isProd ? '/my-machine-assistance-react/' : '/', // GitHub Pages base path
+    base: isProd ? '/my-machine--test-v2/' : '/', // GitHub Pages base path
     build: {
       rollupOptions: {
         output: {
